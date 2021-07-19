56babad30ae4cb3b8236c4f1ca716ac6eb4acc42
DamianoLandi <damianolandi01@gmail.com>
2021-07-19 15:39:47 +0200

    Esercizio 2

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 var firstList = document.getElementById("first-list");
 
 var firstStudent = {
-    Name: "Mario",
-    Surname: "Rossi",
+    Name: "Francesco",
+    Surname: "Viola",
     Age: 20,
 }
 
@@ -15,3 +15,43 @@ for(var key in firstStudent){
 }
 
 firstList.innerHTML = firstStudentDisplay;
+
+//ESERCIZIO 2
+
+var secondList = document.getElementById("second-list");
+
+var studentsList = [
+    {
+        Name: "Mario",
+        Surname: "Rossi",
+        Age: 19,
+    },
+
+    {
+        Name: "Luigi",
+        Surname: "Verdi",
+        Age: 23,
+    }
+    
+    
+]
+
+var studentInput = {
+    Name: prompt("Inserire nome"),
+    Surname: prompt("Inserire cognome"),
+    Age: parseInt(prompt("Inserire età")),
+}
+
+studentsList.push(studentInput);
+
+var secondStudentDisplay = "";
+
+for(var i = 0; i < studentsList.length; i++){
+
+    for(var key in studentsList[i]){
+        secondStudentDisplay += "<li>" + key + ": " + studentsList[i][key] + "</li>"
+    }
+
+}
+
+secondList.innerHTML = secondStudentDisplay;
